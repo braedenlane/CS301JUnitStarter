3e2e111456ddc5a6038281f2a6dc5bcff0b9feb0
Braeden Lane <braedenlane10@gmail.com>
2020-03-23 20:49:11 -0700

    Checkpoint 3.

--- app/src/test/java/up/edu/dimcalc/TwoPointsTest.java
+++ app/src/test/java/up/edu/dimcalc/TwoPointsTest.java
@@ -16,7 +16,7 @@ public class TwoPointsTest {
         Point p2 = testPoints.getPoint(1);
         assertEquals(0, p1.x);
         assertEquals(0, p1.y);
-        assertEquals(1, p2.x);
+        assertEquals(0, p2.x);
         assertEquals(0, p2.y);
     }
 
@@ -31,28 +31,70 @@ public class TwoPointsTest {
         Point p2 = testPoints.getPoint(1);
         assertEquals(5, p1.x);
         assertEquals(-3, p1.y);
-        assertEquals(5, p2.x);
+        assertEquals(-3, p2.x);
         assertEquals(5, p2.y);
     }
 
 
     @Test
     public void randomValue() {
+        TwoPoints testPoints = new TwoPoints();
+        testPoints.randomValue(0);
+        testPoints.randomValue(1);
+        Point p1 = testPoints.getPoint(0);
+        Point p2 = testPoints.getPoint(1);
+        assertEquals(testPoints.getPoint(0).x, p1.x);
+        assertEquals(testPoints.getPoint(0).y, p1.y);
+        assertEquals(testPoints.getPoint(1).x, p2.x);
+        assertEquals(testPoints.getPoint(1).y, p2.y);
     }
 
     @Test
     public void setOrigin() {
+        TwoPoints testPoints = new TwoPoints();
+        testPoints.setPoint(0, 5, -3);
+        testPoints.setPoint(1, -3, 5);
+        Point p1 = testPoints.getPoint(0);
+        Point p2 = testPoints.getPoint(1);
+        testPoints.setOrigin(0);
+        testPoints.setOrigin(1);
+        assertEquals(0, p1.x);
+        assertEquals(0, p1.y);
+        assertEquals(0, p2.x);
+        assertEquals(0, p2.y);
     }
 
     @Test
     public void copy() {
+        TwoPoints testPoints = new TwoPoints();
+        testPoints.setPoint(0, 5, -3);
+        testPoints.setPoint(1, -3, 5);
+        Point p1 = testPoints.getPoint(0);
+        Point p2 = testPoints.getPoint(1);
+        testPoints.copy(0, 1);
+        assertEquals(5, p1.x);
+        assertEquals(-3, p1.y);
+        assertEquals(5, p2.x);
+        assertEquals(-3, p2.y);
     }
 
     @Test
     public void distance() {
+        TwoPoints testPoints = new TwoPoints();
+        testPoints.setPoint(0, 5, -3);
+        testPoints.setPoint(1, -3, 5);
+        Point p1 = testPoints.getPoint(0);
+        Point p2 = testPoints.getPoint(1);
+        assertEquals(11.3137, testPoints.distance(), .0001);
     }
 
     @Test
     public void slope() {
+        TwoPoints testPoints = new TwoPoints();
+        testPoints.setPoint(0, 5, -3);
+        testPoints.setPoint(1, -3, 5);
+        Point p1 = testPoints.getPoint(0);
+        Point p2 = testPoints.getPoint(1);
+        assertEquals(-1, testPoints.slope(), .0001);
     }
 }
